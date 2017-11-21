a70fddf3fd3a088850c2e01c97aef8b846dcef22
Paulo Freitas <paulofreitasb@Hotmail.com>
2017-11-21 18:12:08 -0200

    Correção - Títulos
    Correção nos títulos e remoção de campos desnecessários.

--- Views/home.php
+++ Views/home.php
@@ -30,14 +30,7 @@
       </div>
 
       <div class="title_right">
-        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for...">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="button">Go!</button>
-            </span>
-          </div>
-        </div>
+
       </div>
     </div>
 
@@ -53,22 +46,12 @@
 
               <div class="row x_title">
                 <div class="col-md-6">
-                  <h3>Niveis de felicidade x tristeza - dia</h3>
-                </div>
-                <div class="col-md-6">
-                  <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
-                    <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-                    <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
-                  </div>
+                  <h3>NÍVEIS DE SENTIMENTOS - FELICIDADE x TRISTEZA</h3>
                 </div>
               </div>
-
               <div class="col-md-9 col-sm-9 col-xs-12">
                 <div id="chart_plot_01" class="demo-placeholder"></div>
               </div>
-
-
-
               <div class="clearfix"></div>
             </div>
           </div>
